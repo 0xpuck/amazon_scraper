f66bd2feefc2a3d73e33543f2a8aff95cadcac46
alocparker <alocparker1220@gmail.com>
2023-08-08 09:56:29 +0100

    Add filter_word and price presence checking to eliminate irrelevant results

--- amazon_uk/amazon_uk/spiders/amazon_uk.py
+++ amazon_uk/amazon_uk/spiders/amazon_uk.py
@@ -4,10 +4,12 @@ import scrapy
 class AmazonUKSpider(scrapy.Spider):
     name = 'amazon_uk'
     allowed_domains = ['www.amazon.co.uk']
+    # filter_word = None
 
     def start_requests(self):
         search_term = getattr(self, 'search', 'Intel NUC')  # Default to 'Intel NUC' if no search term is provided
         category = getattr(self, 'category', 'computers')  # Default to 'computers' if no category is provided
+        self.filter_word = getattr(self, 'filter_word', None)  # Default to None if no filter is provided
         url = f'https://www.amazon.co.uk/s?k={search_term}&i={category}'
         yield scrapy.Request(url, callback=self.parse)
 
@@ -16,26 +18,33 @@ class AmazonUKSpider(scrapy.Spider):
         product_elements = response.css('[data-asin]')
         for product_element in product_elements:
             asin = product_element.attrib['data-asin']
-            name = product_element.css('span.a-size-medium.a-color-base.a-text-normal::text').get()
-            price = product_element.css('span.a-price .a-offscreen::text').get()
-            # wE can add more details like SKU, model number, etc. by extracting them from the product page.
-            # Need to inspect the product page and use appropriate CSS selectors to extract the required information.
-            voucher = product_element.css(
-                'span.a-size-base.s-highlighted-text-padding.aok-inline-block.s-coupon-highlight-color::text').get()
+            if asin:
+                name = product_element.css('span.a-size-medium.a-color-base.a-text-normal::text').get()
+                price = product_element.css('span.a-price .a-offscreen::text').get()
+                if name and self.filter_word and self.filter_word.lower() not in name.lower():
+                    continue
+                if not price:
+                    continue
+                else:
+                    voucher = product_element.css(
+                        'span.a-size-base.s-highlighted-text-padding.aok-inline-block.s-coupon-highlight-color::text').get()
 
-            # Extract product link
-            link = product_element.css('a.a-link-normal::attr(href)').get()
-            if link:  # Make the link absolute if it's relative
-                link = response.urljoin(link)
+                    # Extract product link
+                    link = product_element.css('a.a-link-normal::attr(href)').get()
+                    if link:  # Make the link absolute if it's relative
+                        link = response.urljoin(link)
 
-            yield {
-                'asin': asin,
-                'name': name,
-                'price': price,
-                'voucher': voucher if voucher else 'No voucher',
-                'link': link
-                # Add more fields as needed
-            }
+                    yield {
+                        'asin': asin,
+                        'filter': self.filter_word if self.filter_word else 'No filter',
+                        'name': name,
+                        'price': price,
+                        'voucher': voucher if voucher else 'No voucher',
+                        'link': link
+                        # Add more fields as needed
+                    }
+            else:
+                continue
 
         # Follow pagination links if available
         next_page = response.css('a.s-pagination-next::attr(href)').get()
